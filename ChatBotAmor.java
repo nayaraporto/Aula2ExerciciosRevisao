57babcc5623f62d36e163e28b7e1255c1c01d53d
nayaraporto <naya08.vitoria@gmail.com>
2025-08-19 20:52:47 -0300

    Update ChatBotAmor.java

--- ChatBotAmor.java
+++ ChatBotAmor.java
@@ -1,10 +1,28 @@
 package ExerciciosRevisao;
-
+import java.util.Scanner;
 public class ChatBotAmor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Você é legal? (responda 'sim' ou 'não'): ");
+        String resposta = sc.nextLine().toLowerCase(); // deixa minúsculo para facilitar
+
+        if (resposta.equals("sim")) {
+            System.out.println("Você merece um bombom ");
+        } else if (resposta.equals("não") || resposta.equals("nao")) {
+            System.out.println("Todo mundo tem seus dias ");
+        } else {
+            System.out.println("Resposta inválida! Tente responder apenas 'sim' ou 'não'.");
+        }
+
+        sc.close();
+    }
+}
+
 
 	}
 
 }
+
