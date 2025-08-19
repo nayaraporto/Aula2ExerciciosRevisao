185758b9d52fad6c3ed359b29252950515f480e5
nayaraporto <naya08.vitoria@gmail.com>
2025-08-19 20:57:36 -0300

    Update AteTirar6.java

--- AteTirar6.java
+++ AteTirar6.java
@@ -1,10 +1,27 @@
 package ExerciciosRevisao;
-
+		import java.util.Random;
 public class AteTirar6 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+        Random random = new Random();
+        int tentativa = 0;
+        int resultado;
+
+        System.out.println("Rolando o dado até sair 6...\n");
+
+        do {
+            resultado = 1 + random.nextInt(6); 
+            tentativa++;
+            System.out.println("Tentativa " + tentativa + " → saiu " + resultado);
+        } while (resultado != 6);
+
+        System.out.println("\n O número 6 saiu após " + tentativa + " tentativas!");
+    }
+}
+
 
 	}
 
 }
+
