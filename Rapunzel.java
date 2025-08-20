e092759b3ebd3ff3a61b3aaa500126347f9dfbbd
nayaraporto <naya08.vitoria@gmail.com>
2025-08-19 21:16:43 -0300

    Update Rapunzel.java

--- Rapunzel.java
+++ Rapunzel.java
@@ -1,10 +1,33 @@
 package ExerciciosRevisao;
-
+import java.util.Random;
 public class Rapunzel {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+        Random random = new Random();
+        int totalDegraus = 10;
+        int degrauRapunzel = 1 + random.nextInt(totalDegraus); 
+
+        int degrauAtual = 0;
+
+        while (degrauAtual < totalDegraus) {
+            degrauAtual++;
+            System.out.println("Subindo degrau " + degrauAtual + "...");
+
+            if (degrauAtual == degrauRapunzel) {
+                System.out.println("Você encontrou Rapunzel no degrau " + degrauAtual + "!");
+                break;
+            }
+        }
+
+        if (degrauAtual < degrauRapunzel) {
+            System.out.println("O personagem chegou ao topo, mas Rapunzel não estava lá.");
+        }
+    }
+}
+
 
 	}
 
 }
+
